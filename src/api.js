463db119bc8a2f6685d2bdd3669df9f09069f45d
Ilya Koshelev <mifomen@yandex.ru>
2022-03-26 00:03:50 +0300

    bugs

--- src/api.js
+++ src/api.js
@@ -1,1 +1,9 @@
+const data = {
+  qustionText: 'fio',
+  questionImg: 'test',
+  questionAudio: 'name',
+  arrayAnswers: ['test', 'otvet2','otver3','otet4']
+}
+console.log('tet123')
+console.log(`arrayAnswers=${data.arrayAnswers}`)
 console.log('start') //eslint-disable-line
